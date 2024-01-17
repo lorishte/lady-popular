f3b9828b98418baf507a1516242c882ac95f95d9
lorishte <lora.milcheva@outlook.com>
2024-01-17 11:59:26 +0200

    Game Rounds logic

--- src/components/layout/LeftBoard.jsx
+++ src/components/layout/LeftBoard.jsx
@@ -7,6 +7,10 @@ const LeftBoard = () => {
     const [resourceQuantity, setResourceQuantity] = useState(1)
     const [resourceName, setResourceName] = useState('coins')
     const [openTilesIndexes, setOpenTilesIndexes] = useState([])
+    const [currentRoundNumber, setCurrentRoundNumber] = useState(1)
+
+    const NUMBER_OF_ROUNDS = 6
+    const resetIsAllowed = currentRoundNumber === NUMBER_OF_ROUNDS || openTilesIndexes.length < 9
 
 
     const openTile = (e) => {
@@ -39,15 +43,29 @@ const LeftBoard = () => {
     }
 
     const resetBoard = () => {
+        if (resetIsAllowed) return
+
         setOpenTilesIndexes([])
         setResourceQuantity(1)
         setResourceName('coins')
+        setCurrentRoundNumber(currentRoundNumber + 1)
         shuffleArray(ITEMS)
     }
+
+    const rounds = []
+    for (let i = 1; i <= NUMBER_OF_ROUNDS; i++) {
+        const isActive = i === currentRoundNumber
+        rounds.push(<div className={isActive ? 'round-icon active' : 'round-icon'}/>)
+    }
+
 
     return (
         <div className='left-board'>
             <h2>Open 3 matching tiles and you win the item!</h2>
+
+            <div className='rounds'>
+                {rounds}
+            </div>
 
             <div className='left-board__game'>
                 {ITEMS.map((item, index) =>
@@ -63,7 +81,7 @@ const LeftBoard = () => {
 
             <div className='left-board__footer'>
                 <p>Board will reset in: <span className='left-board__timer'>01:23:54</span></p>
-                <button className='btn btn--primary icon' onClick={resetBoard}>
+                <button className='btn btn--primary icon' onClick={resetBoard} disabled={resetIsAllowed}>
                     <span className='btn__icon'>
                         <img src={'/images/diamond.png'} alt='diamond resource'/>3
                     </span>
