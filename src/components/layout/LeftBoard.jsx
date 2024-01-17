1f1eaaa9953fe9b1d3057a0782dda23e6ed13ca8
lorishte <lora.milcheva@outlook.com>
2024-01-17 11:31:22 +0200

    Flip game tiles

--- src/components/layout/LeftBoard.jsx
+++ src/components/layout/LeftBoard.jsx
@@ -10,8 +10,14 @@ const LeftBoard = () => {
 
 
     const openTile = (e) => {
-        const index = Number(e.target.attributes['data-index'].value)
+        const dataIndex = e.target.attributes['data-index']
+
+        if (!dataIndex) return
+
+        const index = Number(dataIndex.value)
+
         if (openTilesIndexes.length >= 9 || openTilesIndexes.includes(index)) return
+
         const updatedIndexes = [...openTilesIndexes, index]
         setOpenTilesIndexes(updatedIndexes)
     }
@@ -19,13 +25,18 @@ const LeftBoard = () => {
     useEffect(() => {
         const numberOfTilesOpened = openTilesIndexes.length
         if (numberOfTilesOpened >= 6) {
-            setResourceQuantity(3)
-            setResourceName('diamond')
+            changeResources('diamond', 3)
         } else if (numberOfTilesOpened >= 3) {
-            setResourceQuantity(2)
-            setResourceName('emerald')
+            changeResources('emerald', 2)
         }
     }, [openTilesIndexes])
+
+    const changeResources = (type, quantity) => {
+        setTimeout(() => {
+            setResourceQuantity(quantity)
+            setResourceName(type)
+        }, 500)
+    }
 
     const resetBoard = () => {
         setOpenTilesIndexes([])
@@ -51,7 +62,7 @@ const LeftBoard = () => {
             </div>
 
             <div className='left-board__footer'>
-                <p>Board will reset in: <span className='left-board__timer'>01:23:54</span> </p>
+                <p>Board will reset in: <span className='left-board__timer'>01:23:54</span></p>
                 <button className='btn btn--primary icon' onClick={resetBoard}>
                     <span className='btn__icon'>
                         <img src={'/images/diamond.png'} alt='diamond resource'/>3
--- src/components/ui/GameTile.jsx
+++ src/components/ui/GameTile.jsx
@@ -4,35 +4,44 @@ const GameTile = (props) => {
     const { element, index, isOpen, resourceName, resourceQuantity, openTile } = props
     const resourceImage = GAME_ASSETS[resourceName].imageName
 
-    if (isOpen) {
-        const assetImage = element.imageName
-        const assetQuantity = element.quantity
-
-        return (
-            <div className='game-tile open'>
-                <div className='game-tile__asset-image'>
-                    <img src={'/images/game-assets/' + assetImage} alt={assetImage}/>
-                </div>
-                <span className='game-tile__asset-quantity'>{assetQuantity}</span>
-            </div>
-        )
-    }
-
+    const assetImage = element.imageName
+    const assetQuantity = element.quantity
 
     return (
-        <div className='game-tile'
+        <div className={isOpen ? 'game-tile open' : 'game-tile'}
              data-index={index}
              onClick={(e) => openTile(e)}>
-            <div className='game-tile__resource-image'
+            <div className='game-tile__inner'
                  data-index={index}
                  onClick={(e) => openTile(e)}>
-                <img src={'/images/' + resourceImage}
-                     alt={resourceImage}
+
+                <div className='game-tile__front'
                      data-index={index}
-                     onClick={(e) => openTile(e)}/>
+                     onClick={(e) => openTile(e)}>
+
+                    <div className='game-tile__resource-image'
+                         data-index={index}
+                         onClick={(e) => openTile(e)}>
+
+                        <img src={'/images/' + resourceImage}
+                             alt={resourceImage}
+                             data-index={index}
+                             onClick={(e) => openTile(e)}/>
+                    </div>
+                    <div className='game-tile__resource-quantity' data-index={index}
+                         onClick={(e) => openTile(e)}>{resourceQuantity}</div>
+                </div>
+
+                <div className='game-tile__back'>
+                    <div className='game-tile open'>
+                        <div className='game-tile__asset-image'>
+                            <img src={'/images/game-assets/' + assetImage} alt={assetImage}/>
+                        </div>
+                        <span className='game-tile__asset-quantity'>{assetQuantity}</span>
+                    </div>
+                </div>
             </div>
-            <div className='game-tile__resource-quantity' data-index={index}
-                 onClick={(e) => openTile(e)}>{resourceQuantity}</div>
+
         </div>
     )
 }
